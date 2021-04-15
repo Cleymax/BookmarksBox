977615e3d8c093adca3797139c92cf2c3cf1eb6e
Clément P. (Cleymax) <clemperrin1@gmail.com>
2021-04-15 18:55:03 +0200

    :rocket: Add where for regex on params in Router

--- app/router/Router.php
+++ app/router/Router.php
@@ -38,14 +38,28 @@ class Router
                 $request_type = $_SERVER['REQUEST_METHOD'];
                 $method = $route->getMethods();
                 if ((is_array($method) && in_array($request_type, $method)) || (is_string($method) && strtoupper($method) == strtoupper($request_type))) {
-                    preg_match_all('/{.+}/', $route->getUri(), $mm);
-                    $path = preg_replace('/{.+}/', '([^/]+)', $route->getUri());
+                    $parameters = [];
+                    preg_match_all('/{(.+?)}/', $route->getUri(), $mm, PREG_SET_ORDER);
+                    $path = preg_replace('/{(.+?)}/', '([^/]+)', $route->getUri());
                     if (preg_match("#^$path$#i", $_GET['p'], $mmm)) {
+                        array_shift($mmm);
+                        foreach ($mmm as $params) {
+                            $index = array_search($params, $mmm);
+                            array_push($parameters, [$mm[$index][1] => $params]);
+                        }
+                        foreach ($route->wheres as $where => $expression) {
+                            foreach ($parameters as $para) {
+                                if (array_key_exists($where, $para)) {
+                                    if (!preg_match("@$expression@", $para[$where])) {
+                                        $this->not_found();
+                                    }
+                                }
+                            }
+                        }
                         if ($route->isAuth() && !Auth::check()) {
                             $this->need_login();
                         }
                         $action = $route->getAction();
-                        $params_count = sizeof($mmm);
                         if (is_array($action)) {
                             $action_size = sizeof($action);
                             if ($action_size >= 2) {
@@ -54,10 +68,9 @@ class Router
                                 require_once ROOT_PATH . '/app/controllers/' . $controller_path . '.php';
                                 $controller = new $controller_path();
                                 if (method_exists($controller, $function)) {
-                                    if ($params_count == 1) {
+                                    if (empty($mmm)) {
                                         $controller->$function();
                                     } else {
-                                        unset($mmm[0]);
                                         $return = call_user_func_array([$controller, $function], array_values($mmm));
                                         if (is_string($return)) {
                                             echo $return;
@@ -68,10 +81,9 @@ class Router
                                 }
                             }
                         } else {
-                            if ($params_count == 1) {
+                            if (empty($mmm)) {
                                 $action();
                             } else {
-                                unset($mmm[0]);
                                 $return = call_user_func_array($action, array_values($mmm));
                                 if (is_string($return)) {
                                     echo $return;
@@ -91,12 +103,10 @@ class Router
     private function need_login()
     {
         http_response_code(401);
-        dd("Need authentificaiton");
     }
 
     private function not_found()
     {
         http_response_code(404);
-        dd("Page pas trouvé mec !");
     }
 }
