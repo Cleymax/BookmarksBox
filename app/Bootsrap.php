2fe4b4db90c6a2c67dcfc4430aa1e08b808ed1b0
Clément P. (Cleymax) <clemperrin1@gmail.com>
2021-04-16 17:28:56 +0200

    :tada: Add routes, edit routeur, add controllers

--- app/Bootsrap.php
+++ app/Bootsrap.php
@@ -7,7 +7,7 @@ use Whoops\Run;
 
 spl_autoload_register(function ($class_name) {
     $class_name = str_replace("\\", DIRECTORY_SEPARATOR, $class_name);
-    require_once ROOT_PATH . '/../' . $class_name . '.php';
+    require_once ROOT_PATH . '/../' . strtolower($class_name) . '.php';
 });
 
 $whoops = new Run;
---
+++ app/controllers/BookmarkController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Controller;
+
+class BookmarkController extends Controller
+{
+
+}
