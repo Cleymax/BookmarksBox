cf1585a7179a0e90561bb7abd61eea0519b5fc96
Clément P. (Cleymax) <clemperrin1@gmail.com>
2021-04-24 02:45:09 +0200

    🚀 Edit js file name html body

--- resources/layouts/parts/footer.php
+++ resources/layouts/parts/footer.php
@@ -3,7 +3,6 @@
         <span>Par Clément PERRIN et Raphaël HIEN avec <span class="material-icons">favorite</span></span>
     </footer>
     <!-- js -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script async src="<?= $_ENV['BASE_URL'] ?>/js/main.js"></script>
+    <script async src="<?= $_ENV['BASE_URL'] ?>/js/app.js"></script>
 </body>
 </html>
