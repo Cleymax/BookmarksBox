403d65767beb473c55f0ca125b4f139f45ded29e
Raphaël "Zerka" H <33027468+Zerka30@users.noreply.github.com>
2021-04-28 10:16:45 +0200

    🚀 Register function

--- app/security/Auth.php
+++ app/security/Auth.php
@@ -10,20 +10,50 @@ use Sonata\GoogleAuthenticator\GoogleAuthenticator;
 class Auth
 {
     private const SESSION_NAME = 'RT';
-    private const PASSWORD_BCRYPT_COST = 12;
 
-    public static function register()
+    public static function register(string $mail, string $username, string $password, string $confirm): bool
     {
+        if($_POST['password'] != $_POST['confirm']){
+            FlashService::error("Votre mot de passe n'est pas identique");
+        }
 
+        preg_match('/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/', $password, $matches, PREG_OFFSET_CAPTURE);
+        if (empty($matches)) {
+            throw new AuthException('Votre mot de passe doit faire 6 caractères avec un chiffre et une majuscule !');
+        }else{
+            $query = (new Query())
+                ->select("id", "email", "username")
+                ->from("users")
+                ->where("email = ? or username = ?")
+                ->limit(1)
+                ->params([$mail, $username]);
+            
+            $count = $query->rowCount();
+
+            if($count == 1){
+                throw new AuthException('Votre mail ou votre nom d\' utilisateur est déjà utilisé');
+            }
+
+            $password = password_hash($_ENV['SALT'] . $password, PASSWORD_BCRYPT);
+
+            $query = (new Query())
+                ->insert("email", "username", "password")
+                ->into("users")
+                ->values([$mail, $username, $password])
+                ->returning('id');
+
+            $response = $query->first();
+
+            $_SESSION['user'] = array(
+                'id' => $response->id,
+                'email' => $mail,
+                'logged' => true
+            );
+
+            return true;
+        }     
     }
 
-    /**
-     * @param string $email
-     * @param string $password
-     * @param bool $remember
-     * @return bool
-     * @throws \App\Security\AuthException
-     */
     public static function login(string $email, string $password, bool $remember = false): bool
     {
         preg_match('/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/', $password, $matches, PREG_OFFSET_CAPTURE);
